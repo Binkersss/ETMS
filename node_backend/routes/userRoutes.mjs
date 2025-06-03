ea22ebec87f1f9abe3945070d6111dfe2c2aa898
Nathaniel Chappelle <binkersss@user.noreply.github.com>
2025-06-03 13:00:16 -0700

    Restructuring

--- node_backend/routes/userRoutes.mjs
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getUserById, updateUser } from '../controllers/userController.mjs';
-
-const router = express.Router();
-
-router.get('/:id', getUserById);
-router.put('/:id', updateUser);
-
-export default router;
